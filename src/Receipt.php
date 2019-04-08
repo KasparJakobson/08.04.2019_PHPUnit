f3cfde542c8a1fecb027b4dd746296091af7ee5e
Kaspar Jakobson <kaspar.jakobson@khk.ee>
2019-04-08 10:21:17 +0300

    03 Write a TDD method 01_08

--- src/Receipt.php
+++ src/Receipt.php
@@ -7,4 +7,9 @@ class Receipt
     {
         return array_sum($items);
     }
+
+    public function tax($amount, $tax)
+    {
+        return ($amount * $tax);
+    }
 }
--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace TDD\Test;
-require "vendor/autoload.php";
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
@@ -31,6 +31,21 @@ class ReceiptTest extends TestCase
             15, // expected value
             $output, // value returned by total()
             'When summing the total should equal 15' // message to return in case of error
+        );
+    }
+
+    public function testTax()
+    {
+        // Set variables that will be given to tax()
+        $inputAmount = 10.00;
+        $taxInput = 0.10;
+
+        // Run tax() with the variables and save to $output
+        $output = $this->Receipt->tax($inputAmount, $taxInput);
+        $this->assertEquals(
+            1.00, // expected value
+            $output, // value returned by tax()
+            'The tax calculation should equal 1.00' // message to return in case of error
         );
     }
 }

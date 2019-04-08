2a935d493b41602a1372f1232dfa482585ba3dc7
Kaspar Jakobson <kaspar.jakobson@khk.ee>
2019-04-08 10:14:46 +0300

    02 Extend a unit test 01_07

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -8,16 +8,28 @@ use TDD\Receipt;
 
 class ReceiptTest extends TestCase
 {
+    public function setUp()
+    {
+        // Initialise the receipt object
+        $this->Receipt = new Receipt();
+    }
+
+    public function tearDown()
+    {
+        // Delete the receipt object
+        unset($this->Receipt);
+    }
+
     public function testTotal()
     {
+        // Create a variable of an array
+        $input = [0, 2, 5, 8];
 
-        // Initialise the Receipt object
-        $Receipt = new Receipt();
-
-        // Test if the total() method returns the expected value of 15
+        // Give the $input variable to the total() method and save the output to a new variable
+        $output = $this->Receipt->total($input);
         $this->assertEquals(
             15, // expected value
-            $Receipt->total([0, 2, 5, 8]), // actual value returned by method
+            $output, // value returned by total()
             'When summing the total should equal 15' // message to return in case of error
         );
     }

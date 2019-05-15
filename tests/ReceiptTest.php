2e24155fc23e9f1ebc1fb2d6963ce43cb141c7c4
Kaspar Jakobson <kaspar.jakobson@khk.ee>
2019-05-15 14:14:29 +0300

    08 Data providers 02_06; 09 Advanced data providers 02_07; 10 Write an exception-based test 03_01

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -20,19 +20,34 @@ class ReceiptTest extends TestCase
         unset($this->Receipt);
     }
 
-    public function testTotal()
+    /**
+     * @dataProvider provideTotal
+     */
+
+    // $items and $expected are passed here from provideTotal which is the dataProvider for this test
+    public function testTotal($items, $expected)
     {
         // Create a variable of an array and the dummy coupon value
         $input = [0, 2, 5, 8];
         $coupon = null;
 
         // Give the $input variable to the total() method and save the output to a new variable
-        $output = $this->Receipt->total($input, $coupon);
+        $output = $this->Receipt->total($items, $coupon);
         $this->assertEquals(
-            15, // Expected value
+            $expected, // Expected value
             $output, // Value returned by total()
             'When summing the total should equal 15' // Message to return in case of error
         );
+    }
+
+    public function provideTotal()
+    {
+        // Define 2 variables per row that can be passed on to a test
+        return [
+            [[1, 2, 5, 8], 16],
+            [[-1, 2, 5, 8], 14],
+            [[1, 2, 8], 11]
+        ];
     }
 
     public function testTotalAndCoupon()
@@ -50,8 +65,24 @@ class ReceiptTest extends TestCase
         );
     }
 
+    public function testTotalException()
+    {
+        // Create a variable of an array and the coupon value
+        $input = [0,2,5,8];
+        $coupon = 1.20;
+
+        // Make the test expect a certain type of exception
+        $this->expectException('BadMethodCallException');
+        $this->Receipt->total($input, $coupon);
+    }
+
     public function testPostTaxTotal()
     {
+        // Create a variable of an array, the tax and a dummy coupon
+        $items = [1, 2, 5, 8];
+        $tax = 0.20;
+        $coupon = null;
+
         // Create a mock receipt class
         $Receipt = $this->getMockBuilder('TDD\Receipt')
             ->setMethods(['tax', 'total'])
